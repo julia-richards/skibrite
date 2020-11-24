1e7a6ac260c19a378eb6502315d4d47c1bc227a3
julia-richards <julia.m.richards@gmail.com>
2020-11-23 18:57:03 -0700

    renders list of categories on home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Layout from "../Layout";
 import { ReactComponent as ImageBanner } from "../../images/allEvents.svg";
 import "./HomePage.css";
+import * as categoryActions from "../../store/categories";
 
+const HomePage = () => {
+	const dispatch = useDispatch();
+	const categories = useSelector((state) => state.categories);
+	useEffect(() => {
+		dispatch(categoryActions.fetchCategoriesIfNeeded());
+	}, [categories]);
 
-const HomePage = () => (
-	<Layout>
-		<div className="home-banner">
-			<p className="home-banner__text">
-				Find your next nordic ski adventure
-			</p>
-			<ImageBanner
-				alt="nordic-skiing-events"
-				className="home-banner__image"
-			/>
-		</div>
-		<div className="event-category-links">
-			<h2>Browse events by category</h2>
-			<div>
-				<span>Nordic</span>
+	return (
+		<Layout>
+			<div className="home-banner">
+				<p className="home-banner__text">
+					Find your next nordic ski adventure
+				</p>
+				<ImageBanner
+					alt="nordic-skiing-events"
+					className="home-banner__image"
+				/>
 			</div>
-		</div>
-	</Layout>
-);
+			<div className="event-category-links">
+				<h2>Browse events by category</h2>
+				<div>
+					<ul>
+						{categories.map((cat) => (
+							<li key={cat.id}>{cat.name}</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</Layout>
+	);
+};
 
 export default HomePage;
