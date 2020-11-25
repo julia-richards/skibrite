6b647a1c6d8e3bed841171448816611886e5ae08
julia-richards <julia.m.richards@gmail.com>
2020-11-24 17:40:55 -0700

    restyles event page

--- frontend/src/components/EventPage/index.js
+++ frontend/src/components/EventPage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Layout from "../Layout";
 import MapDisplay from "../MapDisplay";
+import DefaultDescription from "./DefaultDescription";
 import { ReactComponent as ImageBanner } from "../../images/alpine.svg";
 import * as eventActions from "../../store/events";
 import "./EventPage.css";
@@ -30,29 +31,66 @@ const EventPage = (props) => {
 
 	return (
 		<Layout>
-			<div className="event-banner">
-				<p className="event-banner__text">
-					<h1>{event.name}</h1>
-				</p>
-				<ImageBanner
-					alt="alpine-skier"
-					className="event-banner__image"
-				/>
+			<div className="event-page">
+				<div className="event-banner">
+					<p className="event-banner__text">
+						<h1>{event.name}</h1>
+						<h4>{event.EventCategory.name}</h4>
+					</p>
+					<ImageBanner
+						alt="alpine-skier"
+						className="event-banner__image"
+					/>
+				</div>
+				<div className="event-details">
+					<div className="event-details__description">
+						<strong>Description</strong>
+						{!!event.description ? (
+							<p>{event.description}</p>
+						) : (
+							<DefaultDescription />
+						)}
+					</div>
+					<div className="event-details__details">
+						<p>
+							<strong>State</strong> {event.state}
+						</p>{" "}
+						<p>
+							<strong>Website</strong>{" "}
+							{
+								<a target="_blank" href={event.website}>
+									{event.website}
+								</a>
+							}
+						</p>
+						<p>
+							<strong>Start Date</strong>{" "}
+							{new Date(event.startsAt).toLocaleDateString(
+								undefined,
+								{
+									weekday: "long",
+									year: "numeric",
+									month: "long",
+									day: "numeric",
+								}
+							)}
+						</p>
+						<p>
+							<strong>End Date</strong>{" "}
+							{new Date(event.endsAt).toLocaleDateString(
+								undefined,
+								{
+									weekday: "long",
+									year: "numeric",
+									month: "long",
+									day: "numeric",
+								}
+							)}
+						</p>
+						<MapDisplay lat={event.lat} long={event.long} />
+					</div>
+				</div>
 			</div>
-
-			<h2>TODO: add event category name via backend</h2>
-			<p>
-				Website:{" "}
-				{
-					<a target="_blank" href={event.website}>
-						{event.website}
-					</a>
-				}
-			</p>
-			<p>Starts At: {new Date(event.startsAt).toLocaleString()}</p>
-			<p>Ends At: {new Date(event.endsAt).toLocaleString()}</p>
-			<p>State: {event.state}</p>
-			<MapDisplay lat={event.lat} long={event.long} />
 		</Layout>
 	);
 };
