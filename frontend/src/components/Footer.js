9237cf77f86231e278f0ff1d6b2f87b5e0a073dc
julia-richards <julia.m.richards@gmail.com>
2020-11-29 23:09:51 -0700

    adds links to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -20,6 +20,26 @@ const Footer = () => {
 			>
 				All rights reserved {new Date().getFullYear()}
 			</p>
+			<div
+				style={{
+					fontSize: 22,
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<a href="https://github.com/julia-richards">
+					<i
+						style={{ color: "var(--light-blue)", margin: "0 10px" }}
+						className="fab fa-github"
+					></i>
+				</a>
+				<a href="https://www.linkedin.com/in/julia-richards-09b5b1b0/">
+					<i
+						style={{ color: "var(--light-blue)", margin: "0 10px" }}
+						className="fab fa-linkedin"
+					></i>
+				</a>
+			</div>
 		</footer>
 	);
 };
