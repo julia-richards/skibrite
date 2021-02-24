c651c3c24efb683705f4ee74b64aad0af8a9c802
julia-richards <julia.m.richards@gmail.com>
2021-02-23 20:45:13 -0700

    removes redux state from localStorage after 10 min

--- frontend/src/localStorage.js
+++ frontend/src/localStorage.js
@@ -1,10 +1,17 @@
+const TEN_MINUTES = 600000;
+
 export const loadState = () => {
   try {
     const serializedState = localStorage.getItem("skibrite-redux-state");
     if (serializedState === null) {
       return undefined;
     }
-    return JSON.parse(serializedState);
+    const { state, lastSavedAt } = JSON.parse(serializedState);
+    // if last saved longer than 10 mins ago
+    if (!lastSavedAt || lastSavedAt < Date.now() - TEN_MINUTES) {
+      return undefined;
+    }
+    return state;
   } catch (err) {
     return undefined;
   }
@@ -12,7 +19,10 @@ export const loadState = () => {
 
 export const saveState = state => {
   try {
-    const serializedState = JSON.stringify(state);
+    const serializedState = JSON.stringify({
+      state,
+      lastSavedAt: Date.now(),
+    });
     localStorage.setItem("skibrite-redux-state", serializedState);
   } catch (err) {
     // Ignore write errors.
